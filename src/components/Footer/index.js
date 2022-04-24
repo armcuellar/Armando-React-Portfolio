9cf34c9fb131f2a52be921c66caec8e11c855c77
Armando <cuelarmando@yahoo.com>
2022-04-23 21:08:52 -0400

    added resume section

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,28 @@ import React from "react";
 function Footer() {
     return (
         <section>
-
+            <div className="d-flex w-100 ">
+                <ul className="mx-auto">
+                    <li className="list-inline-item"><a href="mailto:Cuelarmando@yahoo.com"><img
+                        src={require(`../../assets/images/mail.png`)}
+                        className="img-fluid rounded d-block logo"
+                        alt='LinkedIn Profile'
+                        key='LinkedIn Profile'
+                    /></a></li>
+                    <li className="list-inline-item "><a href="https://github.com/armcuellar" target="_blank" rel="noreferrer"><img
+                        src={require(`../../assets/images/GitHub-Mark-64px.png`)}
+                        className="img-fluid rounded d-block logo"
+                        alt='LinkedIn Profile'
+                        key='LinkedIn Profile'
+                    /></a></li>
+                    <li className="list-inline-item "><a href="http://linkedin.com/in/armando-cuellar" target="_blank" rel="noreferrer"><img
+                        src={require(`../../assets/images/In-Blue-Logo.png`)}
+                        className="img-fluid rounded d-block logo"
+                        alt='LinkedIn Profile'
+                        key='LinkedIn Profile'
+                    /></a></li>
+                </ul>
+            </div>
         </section>
     );
 }
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,7 +2,36 @@ import React from "react";
 
 function Resume() {
     return (
-        <section>
+        <section className="mx-3
+        ">
+            <a
+                href="https://drive.google.com/uc?id=1hdNavHnJBFflszckJRjocuchkGgHiZVh&export=download"
+                download
+            >
+                Click to download Resume
+            </a>
+            <div>
+                <h2>Education</h2>
+                <div className="mx-3 px-3">
+                    <h4>College Of Southern Maryland</h4>
+                    <h5>La Plata, MD 20646</h5>
+                    <p>A.A in Information Services Technology</p>
+                </div>
+                <h2>Skills</h2>
+                <ul className="mx-3
+                ">
+                    <li>Bilingual (English/ Spanish)</li>
+                    <li>Customer Service</li>
+                    <li>HTML / CSS</li>
+                    <li>JavaScript</li>
+                    <li>Node.js</li>
+                    <li>Team building</li>
+                    <li>JavaScript API</li>
+                    <li>JavaScript Framework</li>
+                    <li>Adaptability</li>
+                </ul>
+
+            </div>
 
         </section>
     );
